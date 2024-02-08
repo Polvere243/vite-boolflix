7db3e33814507d3a7de96842bb1439e96b46598f
Polvere243 <danielercoli@gmail.com>
2024-02-08 09:06:33 +0100

    installed Font Awesome and created the form, still in progress

--- src/components/BaseForm.vue
+++ src/components/BaseForm.vue
@@ -1,5 +1,18 @@
 <script>
 export default {
-    name: "BaseForm"
+    name: "BaseForm",
+    data: () => ({
+        textToSearch: ""
+    }),
+    props: {
+        defaultText: String
+    }
 }
 </script>
+
+<template>
+    <form>
+        <input type="text" :placeholder="defaultText" v-model="textToSearch">
+        <button>Cerca</button>
+    </form>
+</template>
